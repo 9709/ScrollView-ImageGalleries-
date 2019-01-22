bfddc7b85b3770875805311911ef1bf635c8cb3e
Matthew Chan <mat_t4@hotmail.com>
2019-01-21 16:16:41 -0800

    Initial Commit

---
+++ ScrollView-ImageGalleries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollView-ImageGalleries
+//
+//  Created by Matthew Chan on 2019-01-21.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollView-ImageGalleries/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollView-ImageGalleries
+//
+//  Created by Matthew Chan on 2019-01-21.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
